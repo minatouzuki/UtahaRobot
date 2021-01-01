49a5eb66d04ef726ac9b63806d61b1cec3fea108
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-01 23:26:11 +0530

    Update __main__.py

--- utah/__main__.py
+++ utah/__main__.py
@@ -93,7 +93,7 @@ USER_SETTINGS = {}
 GDPR = []
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("skylee.modules." + module_name)
+    imported_module = importlib.import_module("utah.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 
