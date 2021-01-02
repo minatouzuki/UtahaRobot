86651ba72d95a6a6494e3a7354bd55862c873f70
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-02 16:40:52 +0530

    Update speed_test.py

--- utah/modules/speed_test.py
+++ utah/modules/speed_test.py
@@ -1,7 +1,7 @@
 import speedtest
 from Mizuhararobot import OWNER_ID, dispatcher
 from Mizuhararobot.modules.disable import DisableAbleCommandHandler
-from Mizuhararobot.modules.helper_funcs.chat_status
+from Mizuhararobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
 
